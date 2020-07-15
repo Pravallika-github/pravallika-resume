e337d5c316a395cd6c386f69fd335920b20e82b2
pravallika <pravallikasanapala1@gmail.com>
2020-07-15 13:04:19 +0530

    commited

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl'; 
-import img from '../assets/pravi.jpg'
 
 
 class Landing extends Component {
   render() {
     return(
-      <div style={{width: '100%', margin: 'auto'}}>
-        <Grid className="landing-grid">
-          <Cell col={12}>
-            <img
-              src={img}
-              alt="avatar"
-              className="avatar-img"
-              />
-
-            <div className="banner-text">
-              <h1> SANAPALA PRAVALLIKA</h1>
-                <h3>EMAIL:pravallikasanapalA1@gmail.com</h3>
-                <h3>MOBILE NUMBER:9381935760</h3>
-                <h3>ADDRESS:VISAKHAPATNAM</h3>
-                <hr/>
-            </div>
-          </Cell>
-        </Grid>
-      </div>
+      <body className="landing-grid"><br/><br/><br/><br/><br/>
+          <h1 style={{textAlign:'center',textShadow:' 5px 4px 3px #F8F12B'}}>SANAPALA PRAVALLIKA</h1><br/><br/><br/><br/><br/><br/>
+           <p style={{textAlign:'center',fontFamily:'algerian',fontSize:'50px'}}>E-MAIL:&nbsp;&nbsp;pravallika@gmail.com</p><br/><br/><br/><br/>
+           <p style={{textAlign:'center',fontFamily:'algerian',fontSize:'50px'}}>CONTACT:9381935760</p><br/><br/><br/>
+      </body>
     )
   }
 }
